5331037715ca0b8b52d2ed1e65ec0e3f3dce733f
Tom Grand <thomas.a.grand@gmail.com>
2017-12-11 12:42:14 +0000

    Refactor typos.rb

--- typos.rb
+++ typos.rb
@@ -1,57 +1,66 @@
-
+@students = []
 
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-
-  students = []
+  puts "To finish, just hit return"
   name = gets.chomp
  
   until name.empty? 
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students 
+  @students 
 end
 
-def print_header(students)
-  return false if students.empty? 
+def print_header
+  return false if @students.empty? 
   puts "The students of my cohort at Makers Academy"
   puts "-------------"
 end
 
-def print_students(students)
- return false if students.empty? 
-  students.each do |student|
+def print_student_list
+ return false if @students.empty? 
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  print "Overall, we have #{names.count} great students"
+def print_footer
+  print "Overall, we have #{@students.count} great students"
 end
 
-
-def interactive_menu
-students = []
-loop do 
+def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "9. Exit"
-  selection = gets.chomp.to_i
-  case selection 
-  when 1
-    students = input_students
-  when 2
-    print_header
-    print_students
-    print_footer
-  when 9
-     exit 
-  else 
-    puts "I don't know what you meant, try again"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+  puts
+end
+
+def process(selection)
+   case selection
+     when 1
+     input_students
+     when 2
+     show_students
+     when 9
+     exit
+     else
+     puts "I don't know what you mean, try again"
   end
+end
+
+def interactive_menu
+loop do 
+  print_menu
+  selection = gets.chomp.to_i
+  process(selection)
  end
 end
 
