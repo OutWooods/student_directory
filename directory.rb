f8eeaf0be36d9ae9ced4d87584661fb9b059d71e
Tom Grand <thomas.a.grand@gmail.com>
2017-12-11 07:35:44 +0000

    Put student ames in array and puts using each

--- directory.rb
+++ directory.rb
@@ -1,16 +1,18 @@
-puts "The students of Villains Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall we have"
-print  11
-print " great students"
+
+
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+
+students.each {|name| puts name}
+print "Overall we have #{students.count} great students"
