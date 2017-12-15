d5b5b5686a5e59dabff67e34892a99654c58a3fe
Tom Grand <thomas.a.grand@gmail.com>
2017-12-15 13:11:55 +0000

    Indented logger Exercise 3
    Answer to indented logger exercise 3 from chapter 10
    of Chris Pine's book.

--- Pine_exercises.rb
+++ Pine_exercises.rb
@@ -3,19 +3,29 @@
 #Attempt at the programme logger form Chris Pine
 #https://pine.fm/LearnToProgram/chap_10.html exercise 2
 
+@padding = ""
 
 def programme_logger (programme,proc)
-   puts "#{programme} has started at #{Time.now}"
+   padding_length = 2
+   puts "#{@padding}#{programme} has started at #{Time.now}"
+   @padding += " " * padding_length
    proc.call
-   puts "#{programme} has finished at #{Time.now}"
+   @padding.slice!(0,padding_length)
+   puts "#{@padding}#{programme} has finished at #{Time.now}"
+ 
 end
 
-multiply = Proc.new do 
-	        (1..1000).reduce(&:*) 
+add_to_1000 = Proc.new do 
+	     (1..1000).reduce(&:+)
+	   end
+
+multiply_to_1000 = Proc.new do 
+	       programme_logger("add_to_1000", add_to_1000)
+	       (1..1000).reduce(&:*) 
 	       end
 
 one_to_nine_permutations = Proc.new do 
-	                       programme_logger("multiply", multiply)
+	                       programme_logger("multiply_to_1000", multiply_to_1000)
 	                       [1,2,3,4,5,6,7,8,9].permutation.to_a
 	                       end
 
