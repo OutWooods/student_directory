33e20ddeba77ace5b841361766ca05c9d89d02b5
Tom Grand <thomas.a.grand@gmail.com>
2017-12-15 13:04:18 +0000

    Programme Logger  Ex: 2
    Second attempt at programme logger exercise, with a nested call.
    From Chris Pine chapter 10

--- Pine_exercises.rb
+++ Pine_exercises.rb
@@ -10,8 +10,14 @@ def programme_logger (programme,proc)
    puts "#{programme} has finished at #{Time.now}"
 end
 
+multiply = Proc.new do 
+	        (1..1000).reduce(&:*) 
+	       end
 
-One_to_nine_permutations = Proc.new {[1,2,3,4,5,6,7,8,9,10].permutation.to_a}
+one_to_nine_permutations = Proc.new do 
+	                       programme_logger("multiply", multiply)
+	                       [1,2,3,4,5,6,7,8,9].permutation.to_a
+	                       end
 
 
-programme_logger("Permutations", One_to_nine_permutations)
+programme_logger("Permutations", one_to_nine_permutations)
